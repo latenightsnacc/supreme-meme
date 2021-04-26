a98eaa1ce595cb77722b1a07e4eec3ea1456cbf8
Deborah <54311514+latenightsnacc@users.noreply.github.com>
2021-04-26 08:52:19 +0100

    making basic edits

--- .history/word_jam/lib/main_20210426083700.dart
+++ .history/word_jam/lib/main_20210426083700.dart
@@ -2,15 +2,13 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget {
+class MyApp extends StateLessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: ThemeData(primaryColor: Colors.purple[900]),
       home: Scaffold(
         appBar: AppBar(title: Text('WordJam')),
-        body: Center(child: Text('Hello World')),
-      ),
-    );
+      )
+    )
   }
 }
